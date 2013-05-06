34a1457090cd1dc8a34305e82972ed914df9ea44
Thomas Menguy <thomas.menguy@gmail.com>
2013-05-06 18:52:14 +0200

    Added Business Value BurnUp Charts

--- js/App/Controller.js
+++ js/App/Controller.js
@@ -36,6 +36,26 @@ $(function () {
 
       createBurndownChart(selectListsModel.get("board"), todoLists, doneLists);
    });
+   
+   $("#create-businessvalue-chart button").button();
+   $("#create-businessvalue-chart button").click(function (event) {
+      event.preventDefault();
+
+      var todoLists =
+         _.chain(selectListsModel.get("lists"))
+            .filter(function (list) { return list.get("isTodoList"); })
+            .map(function (list) { return list.get("list").id; })
+            .value();
+
+      var doneLists =
+         _.chain(selectListsModel.get("lists"))
+            .filter(function (list) { return list.get("isDoneList"); })
+            .map(function (list) { return list.get("list").id; })
+            .value();
+
+      createBusinessValueChart(selectListsModel.get("board"), todoLists, doneLists);
+   });   
+   
 
    // Boards
    var selectBoardModel = new App.Models.SelectBoard();
@@ -80,12 +100,72 @@ $(function () {
       selectBoardModel.updateBoards();
    }
 
+
+   function createBusinessValueChart(board, todoLists, doneLists) {
+      $("#businessvalue-chart").empty().append($('<div id="businessvalue-progress"><label>Calculating Business Value BurnUp chart...</label><div class="progressbar"></div></div>'));
+      $("#businessvalue-progress .progressbar").progressbar();
+
+      getTotalPoints(true, board, todoLists.concat(doneLists), function (isBV, totalPoints) {
+         getDailyCounts(isBV, board, todoLists, doneLists, totalPoints, function (dailyCounts) {
+            var step = totalPoints / _.keys(dailyCounts).length;
+            var current = totalPoints;
+            var totalBV = [];
+
+            for (var i = 0; i <= _.keys(dailyCounts).length; i++) {
+            	//this is a straight line
+               totalBV.push(current);
+            }
+
+            new Highcharts.Chart({
+               chart:{
+                  renderTo: "businessvalue-chart",
+                  height:500
+               },
+               title:{
+                  text:"Business Value BurnUp"
+               },
+               tooltip:{
+                  enabled:false
+               },
+               credits:{
+                  enabled:false
+               },
+               plotOptions:{
+                  area:{
+                     marker: {
+                        enabled: false
+                     }
+                  }
+               },
+               yAxis: {
+                  min: 0
+               },
+               legend:false,
+               series:[{
+                  name:"Achieved Business Value",
+                  type:'area',
+                  data:_.values(dailyCounts)
+               }, {
+                  name:"Target",
+                  type:'area',
+                  data:totalBV,
+                  color: "#FF0000",
+                  fillColor: "transparent",
+                  lineWidth: 2,
+
+               }]
+            });
+         });
+      });
+   }
+
+
    function createBurndownChart(board, todoLists, doneLists) {
       $("#burndown-chart").empty().append($('<div id="burndown-progress"><label>Calculating burndown chart...</label><div class="progressbar"></div></div>'));
       $("#burndown-progress .progressbar").progressbar();
 
-      getTotalPoints(board, todoLists.concat(doneLists), function (totalPoints) {
-         getDailyCounts(board, todoLists, doneLists, totalPoints, function (dailyCounts) {
+      getTotalPoints(false, board, todoLists.concat(doneLists), function (isBV, totalPoints) {
+         getDailyCounts(isBV, board, todoLists, doneLists, totalPoints, function (dailyCounts) {
             var step = totalPoints / _.keys(dailyCounts).length;
             var current = totalPoints;
             var countdown = [];
@@ -165,7 +245,7 @@ $(function () {
       return new Date(year, month - 1, day);
    }
 
-   function getCardPoints(card) {
+   function getCardPoints(isBV, card) {
       //var regex = /^(?:\()([\d]+)(?:\)).*/;
       var regex = /.*\(.*BV\:\s*([0-9]+).*SP\:\s*([0-9]+).*\).*/;
       var target = card.name;
@@ -173,15 +253,24 @@ $(function () {
 
       if (target.match(regex)) {
          var result = regex.exec(target);
-         points = parseInt(result[2]);
+         if(isBV){
+         	points = parseInt(result[1]);
+         }else{
+         	points = parseInt(result[2]);
+         }
 
       }
 
       return points;
    }
 
-   function getDailyCounts(board, todoLists, doneLists, totalCards, onComplete) {
-      var progressBar = $("#burndown-progress .progressbar");
+   function getDailyCounts(isBV, board, todoLists, doneLists, totalCards, onComplete) {
+   	var progressBar = {};
+   	  if(isBV){
+      	progressBar = $("#businessvalue-progress .progressbar");
+   	  }else{
+   	  	progressBar = $("#burndown-progress .progressbar");
+   	  }
 
       var statusesByCard = {};
       var dailyCounts = {};
@@ -192,26 +281,39 @@ $(function () {
       var currentDate = new Date(startDate.getTime());
 
       while (currentDate <= endDate) {
-         dailyCounts[getDateString(currentDate)] = totalCards;
+      	if(isBV){
+         	dailyCounts[getDateString(currentDate)] = 0;
+      	}else{
+      		dailyCounts[getDateString(currentDate)] = totalCards;
+      	}
 
          currentDate.setDate(currentDate.getDate() + 1);
       }
 
-      var populateDailyCounts = function () {
+      var populateDailyCounts = function (isBV) {
          _.each(_.values(statusesByCard), function (status) {
-            var points = getCardPoints(status.card);
+            var points = getCardPoints(isBV, status.card);
 
             if (_.any(doneLists, function (list) { return list == status.list })) {
                // This card is completed.
                _.each(_.keys(dailyCounts), function (targetDate) {
                   if ((status.statusOn.length > 0 && targetDate >= status.statusOn) || targetDate < status.createdOn)
-                     dailyCounts[targetDate] -= points;
+                  	if(isBV){
+                  		dailyCounts[targetDate] += points;
+                  	}else{
+                     	dailyCounts[targetDate] -= points;
+                  	}
                });
             }
             else {
                _.each(_.keys(dailyCounts), function (targetDate) {
-                  if (targetDate < status.createdOn)
-                     dailyCounts[targetDate] -= points;
+                  if (targetDate < status.createdOn){
+                  	if(isBV){
+                  		dailyCounts[targetDate] += points;
+                  	}else{
+                     	dailyCounts[targetDate] -= points;
+                  	}
+                  }
                });
             }
          });
@@ -221,9 +323,9 @@ $(function () {
          onComplete(dailyCounts);
       };
 
-      getRelevantCards(board, todoLists.concat(doneLists), function (cards) {
+      getRelevantCards(isBV, board, todoLists.concat(doneLists), function (isBV, cards) {
          if (cards.length == 0) {
-            populateDailyCounts();
+            populateDailyCounts(isBV);
             return;
          }
 
@@ -236,7 +338,7 @@ $(function () {
                progressBar.progressbar("value", Math.round(((cards.length - requestsPending) / cards.length) * 100));
 
                if (requestsPending == 0) {
-                  populateDailyCounts();
+                  populateDailyCounts(isBV);
                }
             })
          });
@@ -282,18 +384,18 @@ $(function () {
       });
    }
 
-   function getTotalPoints(board, lists, onComplete) {
-      getRelevantCards(board, lists, function (cards) {
+   function getTotalPoints(isBV, board, lists, onComplete) {
+      getRelevantCards(isBV, board, lists, function (isBV, cards) {
          var totalPoints =
             _.reduce(cards, function (memo, card) {
-               return memo + getCardPoints(card);
+               return memo + getCardPoints(isBV, card);
             }, 0);
 
-         onComplete(totalPoints);
+         onComplete(isBV, totalPoints);
       });
    }
 
-   function getRelevantCards(board, lists, onComplete) {
+   function getRelevantCards(isBV, board, lists, onComplete) {
       var url = "/boards/" + board + "/cards";
       var options = {
          filter:"open"
@@ -306,7 +408,7 @@ $(function () {
             });
          });
 
-         onComplete(filteredCards);
+         onComplete(isBV, filteredCards);
       });
    }
 });
